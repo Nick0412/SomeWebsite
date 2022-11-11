8fecfa77388c140299ca15073d2feffb21cd1b0b
Nicholas Draper <nickd2225@gmail.com>
2022-11-11 01:37:32 -0600

    Added linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,51 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
       },
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
       Utilities: path.resolve(__dirname, 'src/utilities/'),
-      Components: path.resolve(__dirname, 'src/components/'),
+      Components: path.resolve(__dirname, 'src/components/')
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html'
-    }),
+      template: './src/index.html'
+    })
   ],
   devServer: {
     port: 3000,
     static: {
-        directory: path.join(__dirname, 'src'),
+      directory: path.join(__dirname, 'src')
     }
   }
-};
+}
